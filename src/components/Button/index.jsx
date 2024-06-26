e3a69b9bd39952bcefc12b355f2d4e9938d3fd2c
Vitor Martini <vitor-martini@outlook.com>
2024-06-25 22:16:26 -0300

    fix: set rest on button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import { Container } from "./styles";
 
-export function Button({ title, bgColor, icon: Icon  }) {
+export function Button({ title, bgColor, icon: Icon, ...rest  }) {
   return (
-    <Container bgColor={bgColor}>
+    <Container bgColor={bgColor} { ...rest }>
       { Icon && <Icon />  }
       { title }
     </Container>
