084481a0aa347652598112b2ca72ee90685e2778
Vitor Martini <vitor-martini@outlook.com>
2024-06-25 22:23:07 -0300

    style: sr-only class

--- src/styles/global.js
+++ src/styles/global.js
@@ -36,4 +36,15 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(0.9);
   }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
 `;
