4abd4430682a9067b90b2837f25ee5ca33049a6d
Vitor Martini <vitor-martini@outlook.com>
2024-06-27 23:05:03 -0300

    style: responsive footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.footer`
   display: flex;
@@ -10,14 +11,27 @@ export const Container = styled.footer`
     color: ${({ theme }) => theme.COLORS.LIGHT_200 };
     font-size: 1.4rem;
   }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}px) {
+    > p {
+      font-size: 1.2rem;
+    }
+  }
 `;
 
 export const Logo = styled.div`
   display: flex;
+  align-items: center;
   gap: 1rem;
 
-  >h1 {
+  > h1 {
     color: ${({ theme }) => theme.COLORS.LIGHT_700};
     font-size: 2.4rem;
-}
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}px) {
+    > h1 {
+      font-size: 1.6rem;
+    }
+  }
 `;
