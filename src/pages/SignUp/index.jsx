74518aaccfb77b52e005db68fbec4cd688e7a7c4
Vitor Martini <vitor-martini@outlook.com>
2024-07-02 20:47:19 -0300

    feat: sign up page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,9 +4,37 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 export function SignUp() {
+  const navigate = useNavigate();
   const isMobile = useMediaQuery({ maxWidth: DEVICE_BREAKPOINTS.MD });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSigUp() {
+    if(!name || !email || !password || !email.includes("@")) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+    
+    try {
+      await api.post("/users", { name, email, password });
+      alert("Cadastro efetuado com sucesso!");
+      navigate("/");
+    } catch (error) {
+      if(error.response) {
+        console.log(error);
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar.");
+      }
+    }
+  }
 
   return (
     <Container>
@@ -27,6 +55,7 @@ export function SignUp() {
             label={"Seu nome"}
             inputId={"name"}
             placeholder={"Fulano de Tal"}
+            onChange={e => setName(e.target.value)}
           />
         </InputContainer>
 
@@ -36,6 +65,8 @@ export function SignUp() {
             label={"Email"}
             inputId={"email"}
             placeholder={"exemplo@email.com"}
+            type={"email"}
+            onChange={e => setEmail(e.target.value)}
           />
         </InputContainer>
 
@@ -46,15 +77,17 @@ export function SignUp() {
             inputId={"password"}
             placeholder={"No mínimo 6 caracteres"}
             type={"password"}
+            onChange={e => setPassword(e.target.value)}
           />
         </InputContainer>
 
       <Button
         title={"Criar conta"}
         width={"100%"}
+        onClick={handleSigUp}
       />
 
-      <a href="#">Já tenho uma conta</a>
+      <Link to="/">Já tenho uma conta</Link>
       </CardContainer>
     </Container>
   );
