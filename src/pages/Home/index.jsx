3522ba4814ef5eeda5bfbd8c7f1aa5de64e57cb9
Vitor Martini <vitor-martini@outlook.com>
2024-07-06 18:44:32 -0300

    style: hero

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,20 @@
-import { Container } from "./styles";
+import { Container, HeroContainer, Title, ImageWrapper, Image } from "./styles";
 import { Header } from "../../components/Header";
+import Hero from "../../assets/hero.svg";
 
 export function Home() {
   return (
     <Container>
-      <Header/>
+      <Header />
+      <HeroContainer>
+        <ImageWrapper>
+          <Image src={Hero} />
+        </ImageWrapper>
+        <Title>
+          <h1>Sabores inigualáveis</h1>
+          <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+        </Title>
+      </HeroContainer>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,68 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const HeroContainer = styled.div`
+  width: 100%;
+  background: linear-gradient(180deg, 
+    ${({ theme }) => theme.COLORS.DARK_800} 0%, 
+    ${({ theme }) => theme.COLORS.DARK_900} 50%, 
+    ${({ theme }) => theme.COLORS.DARK_1000} 100%
+  );
+  margin: 8rem 0rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 260px; 
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}px) {
+    height: 200px;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}px) {
+    height: 120px;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const Image = styled.img`
+  height: 120%; 
+`;
+
+export const Title = styled.div`
+  text-align: center;
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    font-weight: 500;
+    font-size: 4rem;
+  }
+
+  > p {
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    font-size: 1.4rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.MD}px) {
+    > h1 {
+      font-size: 2.4rem;
+    }
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.SM}px) {
+    > h1 {
+      font-size: 1.8rem;
+    }
+
+    > p {
+      font-size: 1.2rem;
+    }
+  }
+`;
