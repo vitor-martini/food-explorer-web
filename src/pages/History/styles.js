f2d5404779660d51922f3e71830960f013d5c8d6
Vitor Martini <vitor-martini@outlook.com>
2024-07-11 23:12:29 -0300

    style: making description column bigger

--- src/pages/History/styles.js
+++ src/pages/History/styles.js
@@ -20,29 +20,41 @@ export const Container = styled.div`
     text-align: left;
     padding: 1.6rem 2.4rem;
     font-size: 1.4rem;
+    white-space: nowrap; 
   }
 
   th {
     font-weight: 700;
   }
 
+  th:first-child, td:first-child,
+  th:nth-child(2), td:nth-child(2),
+  th:nth-child(4), td:nth-child(4) {
+    width: 1%;
+    white-space: nowrap;
+  }
+
+  th:nth-child(3), td:nth-child(3) {
+    width: auto; 
+  }
+
   .status-dot {
-      height: 10px;
-      width: 10px;
-      border-radius: 50%;
-      display: inline-block;
-      margin-right: 10px;
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    display: inline-block;
+    margin-right: 10px;
   }
 
   .pendente {
-    background-color: ${({ theme }) => theme.COLORS.TOMATO_300 };
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_300};
   }
 
   .preparando {
-    background-color: ${({ theme }) => theme.COLORS.CARROT_100 };
+    background-color: ${({ theme }) => theme.COLORS.CARROT_100};
   }
 
   .entregue {
-    background-color: ${({ theme }) => theme.COLORS.MINT_100 };
+    background-color: ${({ theme }) => theme.COLORS.MINT_100};
   }
 `;
