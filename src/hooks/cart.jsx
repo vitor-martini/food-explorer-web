1a900435bcb27f9aa9d2ef2aa62e2541858cd0ca
Vitor Martini <vitor-martini@outlook.com>
2024-07-13 08:15:27 -0300

    fix: cart per user

--- src/hooks/cart.jsx
+++ src/hooks/cart.jsx
@@ -1,28 +1,36 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./auth";
 
 export const CartContext = createContext({});
 
 function CartProvider({ children }) {
+  const { user } = useAuth();
   const [cart, setCart] = useState({ order: [] });
 
   function addToCart(dish) {
     const newCart = { order: [...cart.order, dish] };
     setCart(newCart);
-    localStorage.setItem("@food-explorer:cart", JSON.stringify(newCart));
+    localStorage.setItem(`@food-explorer:cart-${user.id}`, JSON.stringify(newCart));
   }
 
   function removeFromCart(index) {
     const newCart = { order: cart.filter((_, i) => i !== index) };
     setCart(newCart);
-    localStorage.setItem("@food-explorer:cart", JSON.stringify(newCart));
+    localStorage.setItem(`@food-explorer:cart-${user.id}`, JSON.stringify(newCart));
   }
 
   useEffect(() => {
-    const localStorageCart = localStorage.getItem("@food-explorer:cart");
+    if(!user || !user.id) {
+      return;
+    }
+
+    const localStorageCart = localStorage.getItem(`@food-explorer:cart-${user.id}`);
     if(localStorageCart) {
       setCart(JSON.parse(localStorageCart));
+    } else {
+      setCart({ order: [] });
     }
-  }, []);
+  }, [user]);
 
   return (
     <CartContext.Provider value={{ addToCart, removeFromCart, cart }}>
